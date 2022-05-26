0e8fbb7374ecc8224ef47bfd225c667cd40aa39c
Lucas Santos <lukas_santos.silva@hotmail.com>
2022-05-26 09:45:03 -0300

    a code to calculate the circumference of a circle

---
+++ circle.py
@@ -0,0 +1,7 @@
+area = int
+perimeter = int
+radius = int(input('Enter the value of radius:' ))
+area = 3.14 * 2
+perimeter = 2 * 3.14 * radius
+print('The area is: ', area)
+print('The perimeter is: ', perimeter)
